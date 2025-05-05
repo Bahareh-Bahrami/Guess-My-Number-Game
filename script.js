5e5a309e81bb8097db92541fd5e97b4ed63b1317
bahareh-bahrami <bahareh.bahramy@gmail.com>
2025-05-05 13:22:45 +0330

    Final Commit

--- script.js
+++ script.js
@@ -45,31 +45,6 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     }
   }
-
-  //   // When guess is too high
-  // } else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📈 Too high!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = '💣 You lost the game!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   }
-
-  //   // When guess is too low
-  // } else {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📉 Too low!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = '💣 You lost the game!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   }
-  // }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
